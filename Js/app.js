daf9915e4d641112badb408969e7e3496b2620dd
emmanuel <sireconu@gmail.com>
2024-11-21 07:03:32 +0100

    Created a congratulation application

---
+++ Js/app.js
@@ -0,0 +1,22 @@
+let mobileMenu = document.querySelector('.mobileButton')
+let mobile = document.querySelector('.mobile')
+
+
+mobileMenu.addEventListener('click', ()=>{
+    mobile.classList.toggle('hidden')
+})
+
+var typed = new Typed("#typed-text", {
+  strings: [
+    "My Sweet Stella Owa Ogiemwonyi, ",
+    "Congratulations on your graduation. Education is the key to unlocking your potential. I pray that God who is your helper and your shield will Continue to help and shield you from all evil Amen!.", "I ❤ You!",
+  ],
+  typeSpeed: 45,
+  backDelay: 1000,
+  backSpeed: 5,
+  loop: true,
+  loopCount: 5,
+});
+
+
+
